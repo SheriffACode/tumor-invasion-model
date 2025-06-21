5334f71821eb30d07de9634b57679c1322cf1bd8
Sheriff Akeeb <sheriffakeeb@gmail.com>
2025-06-21 07:35:01 -0400

    Add files via upload

---
+++ code/Tumor_Spheroid/cclc_math_path/Simulation/CCIecm.py
@@ -0,0 +1,35 @@
+
+from cc3d import CompuCellSetup
+
+        
+#from CCIecmSteppables import NeighborTrackerPrinterSteppable
+
+#CompuCellSetup.register_steppable(steppable=NeighborTrackerPrinterSteppable(frequency=100))
+
+
+from CCIecmSteppables import ConstraintInitializerSteppable
+
+CompuCellSetup.register_steppable(steppable=ConstraintInitializerSteppable(frequency=1))
+
+
+
+
+
+from CCIecmSteppables import GrowthSteppable
+
+CompuCellSetup.register_steppable(steppable=GrowthSteppable(frequency=1))
+
+
+
+
+from CCIecmSteppables import MitosisSteppable
+
+CompuCellSetup.register_steppable(steppable=MitosisSteppable(frequency=1))
+
+
+
+
+
+
+
+CompuCellSetup.run()
